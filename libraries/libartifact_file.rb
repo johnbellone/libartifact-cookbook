ab7c6c69779c2c7490683e59d0500a798a2218fc
John Bellone <jbellone@bloomberg.net>
2015-06-12 11:59:00 -0400

    Fixes a few issue when using with the vault-cookbook.

--- libraries/libartifact_file.rb
+++ libraries/libartifact_file.rb
@@ -13,41 +13,16 @@ class Chef::Resource::LibartifactFile < Chef::Resource
   provides(:libartifact_file)
   actions(:create, :delete)
 
-  attribute(:artifact_name,
-            kind_of: String,
-            name_attribute: true,
-            cannot_be: :empty)
-  attribute(:artifact_version,
-            kind_of: String,
-            required: true)
-  attribute(:install_path,
-            kind_of: String,
-            default: '/srv',
-            cannot_be: :empty)
-  attribute(:remote_url,
-            kind_of: [Array, String],
-            required: true)
-  attribute(:remote_checksum,
-            kind_of: [String, NilClass],
-            default: nil)
-  attribute(:owner,
-            kind_of: [String, NilClass],
-            default: nil)
-  attribute(:group,
-            kind_of: [String, NilClass],
-            default: nil)
-  attribute(:symlink_path,
-            kind_of: [String, NilClass],
-            default: lazy { current_path })
-  attribute(:extract_options,
-            kind_of: [Array, Symbol],
-            default: :no_overwrite)
+  attribute(:artifact_name, kind_of: String, name_attribute: true)
+  attribute(:artifact_version, kind_of: String, required: true)
+  attribute(:install_path, kind_of: String, default: '/srv')
 
-  # The URL where to download the artifact from.
-  # @return [String]
-  def download_url
-    remote_url % { name: artifact_name, version: artifact_version }
-  end
+  attribute(:remote_url, kind_of: [Array, String], required: true)
+  attribute(:remote_checksum, kind_of: String)
+  attribute(:owner, kind_of: String)
+  attribute(:group, kind_of: String)
+  attribute(:symlink_path, kind_of: String, default: lazy { current_path })
+  attribute(:extract_options, kind_of: [Array, Symbol], default: :no_overwrite)
 
   # The absolute path to the artifact's release directory.
   # @return [String]
@@ -61,13 +36,13 @@ class Chef::Resource::LibartifactFile < Chef::Resource
     ::File.join(install_path, artifact_name, 'current')
   end
 
-  # Retrieves the `remote_file` from `download_url`, unpacks it and
-  # creates a symlink to `symlink_path`.
+  # Retrieves the +remote_file+, unpacks it and creates a symlink to
+  # +symlink_path+.
   action(:create) do
     notifying_block do
       include_recipe 'libarchive::default'
 
-      extension = ::File.extname(new_resource.download_url)
+      extension = ::File.extname(new_resource.remote_url)
       friendly_name = "#{new_resource.artifact_name}-#{new_resource.artifact_version}#{extension}"
 
       directory ::File.join(new_resource.install_path, new_resource.artifact_name) do
@@ -76,9 +51,9 @@ class Chef::Resource::LibartifactFile < Chef::Resource
         mode '0755'
       end
 
-      archive = remote_file new_resource.download_url do
+      archive = remote_file new_resource.remote_url do
         path ::File.join(Chef::Config[:file_cache_path], friendly_name)
-        source new_resource.download_url
+        source new_resource.remote_url
         checksum new_resource.remote_checksum
         action :create_if_missing
       end
@@ -97,8 +72,7 @@ class Chef::Resource::LibartifactFile < Chef::Resource
     end
   end
 
-  # Removes the symlink at `symlink_path` and deletes the
-  # directory at `release_path`.
+  # Removes +symlink_path+ and deletes the +release_path+.
   action(:delete) do
     notifying_block do
       link new_resource.symlink_path do
