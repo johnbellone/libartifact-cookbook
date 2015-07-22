8649313ddc39caedbe7abe3ebf90d0c43b2fbe9c
John Bellone <jbellone@bloomberg.net>
2015-07-21 22:18:06 -0400

    Bumps the version of cookbook and requires libarchive ~> 0.6.
    This version of libarchive does not require build-essentials cookbook.

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'jbellone@bloomberg.net'
 license 'Apache 2.0'
 description 'Library cookbook for managing release artifacts.'
 long_description 'Library cookbook for managing release artifacts.'
-version '1.2.1'
+version '1.3.0'
 
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 6.4'
 supports 'redhat', '>= 6.4'
 
-depends 'libarchive'
+depends 'libarchive', '~> 0.6'
 depends 'poise', '~> 2.0'
