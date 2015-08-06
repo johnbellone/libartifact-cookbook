995c0c11cc293ae8619985058758d7df758d3a79
John Bellone <jbellone@bloomberg.net>
2015-08-06 10:15:53 -0400

    Bumps the version of libartifact cookbook.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jbellone@bloomberg.net'
 license 'Apache 2.0'
 description 'Library cookbook for managing release artifacts.'
 long_description 'Library cookbook for managing release artifacts.'
-version '1.3.1'
+version '1.3.2'
 
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 6.4'
