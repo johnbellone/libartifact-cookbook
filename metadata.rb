8169a0e41666d2bca86e3c53f3a1502ab79fa065
John Bellone <jbellone@bloomberg.net>
2015-09-13 11:18:57 -0400

    Removes the matchers.rb since Poise handles it.

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'jbellone@bloomberg.net'
 license 'Apache 2.0'
 description 'Library cookbook for managing release artifacts.'
 long_description 'Library cookbook for managing release artifacts.'
-version '1.3.2'
+version '1.3.3'
 
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 6.4'
 supports 'redhat', '>= 6.4'
 
 depends 'libarchive', '~> 0.6'
-depends 'poise', '~> 2.0'
+depends 'poise', '~> 2.2'
