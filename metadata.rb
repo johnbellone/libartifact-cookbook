634f6f6d8509a2f420d22bd707db596c23f424f3
John Bellone <jbellone@bluedog.engineering>
2016-11-24 09:03:30 -0500

    Bump version for last cookbook release.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jbellone@bloomberg.net'
 license 'Apache 2.0'
 description 'Library cookbook for managing release artifacts.'
 long_description 'Library cookbook for managing release artifacts.'
-version '1.3.4'
+version '1.3.5'
 
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 6.4'
